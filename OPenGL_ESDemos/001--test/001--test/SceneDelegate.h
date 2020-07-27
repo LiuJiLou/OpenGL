330bfde3db892b3e9349922b40ede9b195daafb3
liujilou <liujilou-ds>
2020-07-27 12:50:44 +0800

    OPenGLES

---
+++ OPenGL_ESDemos/001--test/001--test/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  001--test
+//
+//  Created by liujilou on 2020/7/24.
+//  Copyright © 2020 liujilou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ OPenGL_ESDemos/001--test/001--test/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  001--test
+//
+//  Created by liujilou on 2020/7/24.
+//  Copyright © 2020 liujilou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+
+@end
+
